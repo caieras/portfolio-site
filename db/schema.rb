f3f22fab2f93b6652ac3185c3eeddaa2fbaecdb1
c <caio.asevedo@gmail.com>
2022-05-09 18:18:06 -0300

    added portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220509201522) do
+ActiveRecord::Schema.define(version: 20220509210027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +20,16 @@ ActiveRecord::Schema.define(version: 20220509201522) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.text     "main_image"
+    t.text     "thumb_image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "skills", force: :cascade do |t|
