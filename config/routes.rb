9f0eb977c03820e0ac23544e75adce5b8f3de053
c <caio.asevedo@gmail.com>
2022-05-13 15:29:02 -0300

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :portfolios
+  root to: 'pages#home'
+
   resources :blogs
 end
